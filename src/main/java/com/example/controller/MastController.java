0794763f3c346f0a83045ee117c3712c6c05bff4
test <test@test.com>
2019-09-05 11:46:07 +0800

    springbootdemo项目完成一期 master1.1 master1.2

--- src/main/java/com/example/controller/MastController.java
+++ src/main/java/com/example/controller/MastController.java
@@ -1,8 +1,14 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * description:
  * Created by Ray on 2019-09-05
  */
 public class MastController {
+    @RequestMapping("/showhello")
+    public void show(){
+        System.out.println("hello show  master");
+    }
 }
